7b740b2c5946983ef08d49025df25140556daf80
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-01-29 12:59:07 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () { 
+	int cat;
+	cout << " ples give radius of the of the circle " << endl;
+	cin >> cat;
+	cout << 3.14 * cat*cat;
 
-
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+}
